b0f5628fe1a68a3120ef9b5bd8c93c7bb775706b
al <alfredo.yap@stu.bmcc.cuny.edu>
2023-05-31 18:06:00 -0400

    finished assigment5 part a

---
+++ Assignment5/assignment5.js
@@ -0,0 +1,51 @@
+calculate = () => {
+  const inputHeightLucas = document.getElementById('inputHeightLucas');
+  const inputWeightLucas = document.getElementById('inputWeightLucas');
+  const inputHeightPeter = document.getElementById('inputHeightPeter');
+  const inputWeightPeter = document.getElementById('inputWeightPeter');
+
+  const labelBMIResult = document.getElementById('labelBMIResult');
+
+  if (
+    inputHeightLucas.value === '' ||
+    inputWeightLucas.value === '' ||
+    inputHeightPeter.value === '' ||
+    inputWeightPeter.value === ''
+  ) {
+    alert('Please fill all fields');
+    return;
+  } else if (
+    isNaN(inputHeightLucas.value) ||
+    isNaN(inputWeightLucas.value) ||
+    isNaN(inputHeightPeter.value) ||
+    isNaN(inputWeightPeter.value)
+  ) {
+    alert('Please enter numbers only');
+    return;
+  } else {
+    const bmiLucas = calculateBMI(
+      inputHeightLucas.value,
+      inputWeightLucas.value
+    );
+    const bmiPeter = calculateBMI(
+      inputHeightPeter.value,
+      inputWeightPeter.value
+    );
+
+    //return an alert with the result
+
+    console.log(
+      `The BMI of Peter is ${bmiPeter}, and the BMI of Lucas is ${bmiLucas}, Peter's BMI is higher than Lucas' BMI that is ${
+        bmiPeter > bmiLucas
+      }`
+    );
+
+    labelBMIResult.innerHTML = `Lucas BMI: ${bmiLucas} <br> Peter BMI: ${bmiPeter} <br> Lucas BMI is ${
+      bmiLucas > bmiPeter ? 'higher' : 'lower'
+    } than Peter's`;
+  }
+};
+
+calculateBMI = (height, weight) => {
+  return (weight / (height * height)).toFixed(2);
+};

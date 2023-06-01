e05325263194c5e1fa99606522743c6584c1fd71
al <alfredo.yap@stu.bmcc.cuny.edu>
2023-05-31 23:27:40 -0400

    finished assignment 5

--- Assignment5/assignment5.js
+++ Assignment5/assignment5.js
@@ -13,18 +13,18 @@ calculateBMI = () => {
   const peterHeight = inputHeightPeter.value.trim();
 
   if (
-    lucasWeight === '' ||
-    lucasHeight === '' ||
-    peterWeight === '' ||
-    peterHeight === ''
+    isInputEmpty(lucasWeight) ||
+    isInputEmpty(lucasHeight) ||
+    isInputEmpty(peterWeight) ||
+    isInputEmpty(peterHeight)
   ) {
-    alert('Please fill all fields');
+    alert('Please fill all fields for BMI calculation');
     return;
   } else if (
-    isNaN(lucasWeight) ||
-    isNaN(lucasHeight) ||
-    isNaN(peterWeight) ||
-    isNaN(peterHeight)
+    isNotANumber(lucasWeight) ||
+    isNotANumber(lucasHeight) ||
+    isNotANumber(peterWeight) ||
+    isNotANumber(peterHeight)
   ) {
     alert('Please enter numbers only');
     return;
@@ -40,7 +40,7 @@ calculateBMI = () => {
     console.log(partAResult);
     console.log('---------------------------------');
     labelPartAResult.innerHTML =
-      `<b>Part A Result:</b><br> ` + partAResult + `<br>`;
+      `<b>Part A Result:</b><br> ` + partAResult + `<br><br>`;
 
     //Part C Result
     const partCResult = `Lucas' BMI (${bmiLucas}) is ${
@@ -55,6 +55,76 @@ calculateBMI = () => {
   }
 };
 
+ConvertCelsiusToFahrenheit = () => {
+  const inputCelsius = document.getElementById('inputTemperatureCelcius');
+  const labelFahrenheit = document.getElementById(
+    'labelCelsiusTemperatureResult'
+  );
+
+  const celsius = inputCelsius.value.trim();
+
+  if (isInputEmpty(celsius)) {
+    alert('Please enter a value for Celsius');
+    return;
+  } else if (isNotANumber(celsius)) {
+    alert('Please enter numbers only');
+    return;
+  } else {
+    const fahrenheit = getFahrenheit(celsius);
+    const resultFahrenheit = `${celsius}°C is ${fahrenheit}°F`;
+    //Part B Result
+    console.log('---------------------------------');
+    console.log('Parts B & D Result:');
+    console.log(resultFahrenheit);
+    console.log('---------------------------------');
+    labelFahrenheit.innerHTML =
+      `<b>Parts B & D Result:</b><br> ` + resultFahrenheit + `<br><br>`;
+  }
+};
+
+ConvertFahrenheitToCelsius = () => {
+  const inputFahrenheit = document.getElementById('inputTemperatureFahrenheit');
+  const labelCelsius = document.getElementById(
+    'labelFahrenheitTemperatureResult'
+  );
+
+  const fahrenheit = inputFahrenheit.value.trim();
+
+  if (isInputEmpty(fahrenheit)) {
+    alert('Please enter a value for Fahrenheit');
+    return;
+  } else if (isNotANumber(fahrenheit)) {
+    alert('Please enter numbers only');
+    return;
+  } else {
+    const celsius = getCelsius(fahrenheit);
+    const resultCelsius = `${fahrenheit}°F is ${celsius}°C`;
+    //Part B Result
+    console.log('---------------------------------');
+    console.log('Parts B & D Result:');
+    console.log(resultCelsius);
+    console.log('---------------------------------');
+    labelCelsius.innerHTML =
+      `<b>Parts B & D Result:</b><br> ` + resultCelsius + `<br><br>`;
+  }
+};
+
 getBMI = (height, weight) => {
   return (weight / (height * height)).toFixed(2);
+};
+
+isNotANumber = (value) => {
+  return isNaN(value);
+};
+
+isInputEmpty = (value) => {
+  return value === '';
+};
+
+getCelsius = (fahrenheit) => {
+  return ((fahrenheit - 32) * 5) / 9;
+};
+
+getFahrenheit = (celsius) => {
+  return (celsius * 9) / 5 + 32;
 };
